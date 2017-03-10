6485326b6b46e38d5f19d604eab6179cb64ed3c1
christophebedard <chrisvolkoff@gmail.com>
2017-03-10 18:04:40 -0500

    Changed local script to prompt for IPs

--- scripts/rpi_local.sh
+++ scripts/rpi_local.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # arg1 : ROS_MASTER_URI
 # arg2 : ROS_IP
+echo Enter ROS_MASTER_URI
+read varmasteruri
+echo Enter ROS_IP
+read varrosip
+
 source /opt/ros/kinetic/setup.bash
 source ~/roomba_ws/devel/setup.bash
-export ROS_MASTER_URI=$1
-export ROS_IP=$2
+export ROS_MASTER_URI=$varmasteruri
+export ROS_IP=$varrosip
 roslaunch elikos_roomba rpi_local.launch
