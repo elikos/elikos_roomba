863284f05796b9e778f08eb3e3a6a6e37b689a05
christophebedard <chrisvolkoff@gmail.com>
2017-11-20 18:10:50 -0500

    Add line markers

--- src/arena.cpp
+++ src/arena.cpp
@@ -4,13 +4,62 @@
 static const double LOOP_RATE = 1.0;
 static const std::string NODE_NAME = "arena";
 static const std::string TF_MIDDLE_ARENA = "/elikos_arena_origin";
-static const std::string ARENA_SQUARES_TOPIC_NAME = "/arena_squares";
+static const std::string ARENA_SQUARES_TOPIC_NAME = "/arena_markers";
 static const std::string ARENA_SQUARE_MODEL = "package://elikos_roomba/models/arena_square.dae";
 static const double SQUARE_MODEL_OFFSET_X = 0.5;        // Offset to get the "origin" on the (0,0) corner 
 static const double SQUARE_MODEL_OFFSET_Y = 0.5;        // Offset to get the "origin" on the (0,0) corner 
+static const double LINE_WIDTH = 0.1;
 
 visualization_msgs::MarkerArray markerarray_msg;
 ros::Publisher arenasquares_pub;
+
+// Create marker msg for green and red lines
+std::vector<visualization_msgs::Marker> createLineMarkers(int dim) {
+    visualization_msgs::Marker green_line, red_line;
+
+    // green line
+    green_line.header.frame_id = TF_MIDDLE_ARENA;
+    green_line.id = dim*dim + 1;
+    green_line.type = visualization_msgs::Marker::LINE_STRIP;
+    green_line.action = visualization_msgs::Marker::ADD;
+    green_line.mesh_resource = ARENA_SQUARE_MODEL;
+    green_line.pose.orientation.w = 1.0;
+    green_line.scale.x = LINE_WIDTH;
+    green_line.lifetime = ros::Duration();
+    green_line.color.g = 1.0;
+    green_line.color.a = 1.0;
+    geometry_msgs::Point pg1, pg2;
+    pg1.x = - (double)dim / 2;
+    pg1.y = (double)dim / 2;
+    pg2.x = (double)dim / 2;
+    pg2.y = (double)dim / 2;
+    green_line.points.push_back(pg1);
+    green_line.points.push_back(pg2);
+
+    // red line
+    red_line.header.frame_id = TF_MIDDLE_ARENA;
+    red_line.id = dim*dim + 2;
+    red_line.type = visualization_msgs::Marker::LINE_STRIP;
+    red_line.action = visualization_msgs::Marker::ADD;
+    red_line.mesh_resource = ARENA_SQUARE_MODEL;
+    red_line.pose.orientation.w = 1.0;
+    red_line.scale.x = LINE_WIDTH;
+    red_line.lifetime = ros::Duration();
+    red_line.color.r = 1.0;
+    red_line.color.a = 1.0;
+    geometry_msgs::Point pr1, pr2;
+    pr1.x = - (double)dim / 2;
+    pr1.y = - (double)dim / 2;
+    pr2.x = (double)dim / 2;
+    pr2.y = - (double)dim / 2;
+    red_line.points.push_back(pr1);
+    red_line.points.push_back(pr2);
+
+    std::vector<visualization_msgs::Marker> lines;
+    lines.push_back(green_line);
+    lines.push_back(red_line);
+    return lines;
+}
 
 // Create marker msg from (x,y) coords and ID 
 visualization_msgs::Marker createMarkerMsg(double x, double y, int id) {
@@ -38,6 +87,8 @@ visualization_msgs::Marker createMarkerMsg(double x, double y, int id) {
 // Create one-time marker array
 void createMarkerArray(int dim) {
     std::vector<visualization_msgs::Marker> markers;
+
+    // squares
     for (int i = 0; i < dim; ++i) {
         double coord_x = i;
         for (int j = 0; j < dim; ++j) {
@@ -47,6 +98,10 @@ void createMarkerArray(int dim) {
                                                 (i * dim) + j));
         }
     }
+    // lines
+    std::vector<visualization_msgs::Marker> lines = createLineMarkers(dim);
+    markers.insert(markers.end(), lines.begin(), lines.end());
+
     markerarray_msg.markers = markers;
 }
 
