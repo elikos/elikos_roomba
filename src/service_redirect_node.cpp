cd211f3721390c03b0a20cf404b5d9dfee47e06b
christophebedard <chrisvolkoff@gmail.com>
2017-11-23 14:52:57 -0500

    Forgot to !

---
+++ src/service_redirect_node.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include "elikos_roomba/service_redirect.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "serviceredirect");
+    ros::NodeHandle n;
+
+    ServiceRedirect serviceredirect_(n);
+    
+    try
+    {
+        ros::spin();
+    }
+    catch (std::runtime_error& e)
+    {
+        ROS_FATAL_STREAM("[SERVICE REDIRECT] Runtime error: " << e.what());
+        return 1;
+    }
+    return 0;
+}
